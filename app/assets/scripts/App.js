20a7bfd3437dc4042085733282ca09a7391b8380
Pabliz <36200735+pabliz@users.noreply.github.com>
2021-11-12 13:05:37 +0100

    cursor behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -33,7 +33,31 @@ window.addEventListener("load", () => {
 })
 
 // CURSOR
-// const cursor = document.querySelector(".cursor")
+const animateit = function(e) {
+    const span = this.querySelector('span');
+    const { offsetX: x, offsetY: y } = e, { offsetWidth: width, offsetHeight: height } = this,
+
+    move = 8,
+        xMove = x / width * (move * 2) + move,
+        yMove = y / height * (move * 2) - move;
+
+    span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+    if (e.type === 'mouseleave') span.style.transform = '';
+};
+
+
+const link = document.querySelector('.hover-this');
+const cursor = document.querySelector(".cursor")
+const editCursor = e => {
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+};
+
+link.addEventListener('mousemove', animateit);
+link.addEventListener('mouseleave', animateit);
+window.addEventListener('mousemove', editCursor);
 
 // document.addEventListener('mousemove', (e) => {
 //     gsap.to(cursor, {
